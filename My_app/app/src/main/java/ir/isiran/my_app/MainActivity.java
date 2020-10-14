4fcb5511fee58e232568df5f340228ccdde3f05b
Mahdi <mehhdimehhdi@gmail.com>
2020-10-14 15:51:58 +0330

    Android lifecycle log added

--- My_app/app/src/main/java/ir/isiran/my_app/MainActivity.java
+++ My_app/app/src/main/java/ir/isiran/my_app/MainActivity.java
@@ -3,6 +3,7 @@ package ir.isiran.my_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("mytag", "onstart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("mytag", "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("mytag", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("mytag", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("mytag", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("mytag", "onStop");
+    }
 }
+
