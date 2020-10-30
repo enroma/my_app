c0d09ed7a4091daed1c9890386bdfc6941a95912
Mahdi <mehhdimehhdi@gmail.com>
2020-10-30 17:09:09 +0330

    990809 - First commit

---
+++ Profile_app/app/src/main/java/ir/isiran/profile_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.isiran.profile_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
